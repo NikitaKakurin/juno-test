8216a2d16045c19d86b1de2b2254c6629c68fa1e
NikitaKakurin <neyasut87@rambler.ru>
2022-03-27 16:35:40 +0300

    add styles and finish fedback

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './'
+import './style/style.scss';
+
--- webpack.config.js
+++ webpack.config.js
@@ -119,7 +119,7 @@ module.exports = {
           },
           {
             test:/\.(ttf|woff|woff2|eot)$/,
-            type: 'asset/resource'
+            type: "asset/resource"
           },
           {
             test: /\.m?js$/,
